f7cefeafb40ffac879303ae371683657ca39ecad
hashwnath <53908470+hashwnath@users.noreply.github.com>
2020-04-26 18:27:38 +0530

    Update personalAssistant.py

--- personalAssistant.py
+++ personalAssistant.py
@@ -29,7 +29,38 @@ def record_audio(ask = False):
         speak('sorry, I did not get that')
     except sr.RequestError:
         speak('Sorry my speech service is down') 
-    return voice_data       
+    return voice_data          
+def num_maker(wlst):
+    num2words1 = {1: 'one', 2: 'two', 3: 'Three', 4: 'Four', 5: 'Five', \
+            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
+            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
+            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}  
+    intlst = list(num2words1.keys())
+    worlist = list(num2words1.values())        
+    slst = []
+    for i in wlst:
+        if i in worlist:
+            m = intlst[worlist.index(i)]
+            slst.append(m)
+    return slst[0],slst[1]        
+def calc(a,b,voice_data):
+    if 'add' or 'sum' in voice_data:
+        re = a+b
+        speak('the result of addition of'+str(a)+'and'+str(b)+'is'+str(re))
+    if 'subract' or 'difference' in voice_data:
+        re = a-b
+        speak('the result of subraction of'+str(a)+'and'+str(b)+'is'+str(re))
+    if 'product' or 'multiply' in voice_data:
+        re = a*b
+        speak('the result of multiplication of'+str(a)+'and'+str(b)+'is'+str(re))
+    if 'div' in voice_data:
+        re = a/b
+        speak('the result of division of'+str(a)+'and'+str(b)+'is'+str(re))            
+    
+
+
+
+
 def respond():
     if 'what is your name' in voice_data:
         speak('My name is Alexa')
@@ -64,10 +95,40 @@ def respond():
         url = 'https://www.youtube.com/results?search_query='+audio
         webbrowser.get().open(url) 
         speak('please select the video you like...')
-
+    if 'about you' in voice_data:
+        speak('Hi, I am alexa made by Hashwanth Sutharapu in Hyderabad this program is made from basic python and speech recognition modules.It is a great previalge to s[eak to you.')
+    if 'your master' in voice_data:
+        url = 'https://www.google.com/search?q=sutharapu+hashwanth&sxsrf=ALeKk000JAmlSqUKgn5sBdoDuipW_Cnj8w:1587899960903&tbm=isch&source=iu&ictx=1&fir=MElM2qOdXKyCoM%253A%252CU0MEFcq0kZINcM%252C_&vet=1&usg=AI4_-kR2xCe37mitjkNRJEtR0uyPyG9Wxw&sa=X&ved=2ahUKEwjp2u6o_IXpAhVMxDgGHd40CuQQ9QEwBHoECAkQBQ#imgrc=MElM2qOdXKyCoM:'    
+        webbrowser.get().open(url) 
+        speak('I was made by Sutharapu Hashwanth,Here is the image of my master, Hashwanth.')
+        speak('would you like to look at his linkedin profile')
+        agr = record_audio()
+        if 'yes' in agr:
+            url = 'https://www.linkedin.com/in/sutharapuhashwanth/?originalSubdomain=in'    
+            webbrowser.get().open(url)
+            speak('Here is the the linked profile of my master, Hashwanth,would you like to look at his other social media accounts like Youtube, github, blogspot.')
+            agrall = record_audio()
+            if 'yes' in agrall:
+                url = 'https://www.youtube.com/channel/UClRRIYpM3G90naVrJxrVC0w?view_as=subscriber'    
+                webbrowser.get().open(url)
+                speak('Here is the Youtube channel of my master, please have a look at the danger video,Guess who is that, its my master,Do not try this at home')
+                url = 'https://github.com/hashwnath'    
+                webbrowser.get().open(url)
+                speak('And here comes the Github account of my master,check out the netflix clone and the personal_assistant is none other than me.')
+                url = 'https://hashwanth531.blogspot.com/'    
+                webbrowser.get().open(url)
+                speak('This is the blogspot of hashwanth, he put all the cool stuff over there')
+    if 'add' or 'sum' or 'diffrence' or 'subract' or 'product' or 'multiply' or 'div' in voice_data:
+        wlst = voice_data.split()
+        a,b = num_maker(wlst)
+        calc(a,b,voice_data)
+                
+    
 
     if 'exit' in voice_data:
         exit()      
+
+
 speak('Initializing alexa')
 
 for i in range(12345):
